962ab9ac71b544880cc1196079a29fbeeef11a62
Luke Lavin <me@lukelav.in>
2024-11-02 21:07:33 -0500

    overwrite

--- workflows/scrape/refresh_latest_season.py
+++ workflows/scrape/refresh_latest_season.py
@@ -5,8 +5,8 @@ from src.scrape_raw import download_season_list
 from workflows.scrape.shared import refresh_games, refresh_season_list, refresh_seasons
 
 
-@flow(log_prints=True)
-def refresh_all(bucket_name="cluebase", overwrite: bool = False):
+@flow
+def refresh_latest_season(bucket_name="cluebase", overwrite: bool = True):
     if bucket_name:
         print("Refreshing files in bucket: {cluebase}")
 
